08e57be8cbd71db977bf3bd3d891ac3cffb8bcb9
Nikola Ivanovic <nikola.ivanovic@amplitudo.me>
2021-01-25 12:01:24 +0100

    Deployed

--- util/database.js
+++ util/database.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 // const sequelize = new Sequelize("base_project_db", "postgres", "postgres", {
 //   host: "localhost",
